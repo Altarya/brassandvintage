ca52b1d52809d4bfa27abc99c7d269aaae9decdc
cijik <cijik@users.noreply.github.com>
2019-11-13 00:15:20 +0200

    Added rubber tree blocks

--- src/main/java/alty/brassandvintagecore/init/BaVBlocks.java
+++ src/main/java/alty/brassandvintagecore/init/BaVBlocks.java
@@ -11,5 +11,12 @@ public class BaVBlocks {
 
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
-	public static final Block VANADIUM_ORE = new BaVBlockBase("block_vanadium_ore", Material.IRON);
+	public static final Block VANADIUM_ORE = new BaVBlockBase("block_vanadium_ore", Material.ROCK);
+	
+	public static final Block RUBBER_TREE_LOG = new BaVBlockBase("block_rubber_tree_log", Material.WOOD);
+	public static final Block RUBBER_TREE_PLANKS = new BaVBlockBase("block_rubber_tree_planks", Material.WOOD);
+	
+	public static final Block RUBBER_TREE_LEAVES = new BaVBlockBase("block_rubber_tree_leaves", Material.LEAVES);
+	
+	public static final Block RUBBER_TREE_SAPLING = new BaVBlockBase("block_rubber_tree_sapling", Material.PLANTS);
 }
